bf839d5b68ee79ab6bba6d8df3dc26c543241757
samuel ochaba <samuel.ochaba.dev@gmail.com>
2026-09-08 15:41:33 +0100

    app: configure outputFileTracingRoot and includes for content directory

--- platform/app/next.config.ts
+++ platform/app/next.config.ts
@@ -1,7 +1,11 @@
+import path from "node:path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingRoot: path.join(__dirname, "../../"),
+  outputFileTracingIncludes: {
+    "/**": ["../../content/**/*"],
+  },
 };
 
 export default nextConfig;
